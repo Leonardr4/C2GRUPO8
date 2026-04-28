584930c327401e8884af7cd0f004525e167de109
estudianteUC <76151567@continental.edu.pe>
2026-04-28 15:23:08 -0500

    pantallaPrincipal agregada

--- app/src/main/java/com/example/a6new/MainActivity.kt
+++ app/src/main/java/com/example/a6new/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,17 +15,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -33,7 +41,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.initializer
@@ -165,4 +176,46 @@ fun ItemPersona(p: Persona, vm: PersonaViewModel){
 
         }
     }
+}
+@Preview
+@Composable
+fun pantallaPrincipal(){
+    Column(modifier = Modifier.fillMaxSize().background(color = Color.White), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = R.drawable.logo)
+            , contentDescription = "Logo",
+            modifier = Modifier.size(width = 400.dp, height = 400.dp)
+        )
+
+        Row() {
+            Button(
+              onClick =  {}
+            ) {
+                Text(text = "Agregar libros")
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Button(
+                onClick = {}
+            ) {
+                Text(text = "Listar libros")
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+
+
+
+        }
+
+
+        Button(
+            onClick = {},
+
+        ) {
+            //Icon(imageVector = Icons.Default.Close, Color.White)
+            Text(text = "Salir")
+        }
+
+
+    }
+
 }
